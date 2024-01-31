5476164678bd118dc02a53eea1334b992d2c5a57
Luis Fernando Manriquez <luismanriquez13@gmail.com>
2024-01-30 17:18:55 -0700

    testing div100 package

--- src/pages/Layout/main.js
+++ src/pages/Layout/main.js
@@ -1,20 +1,19 @@
 import { Box } from "@mui/material";
 import NavigationBar from "./NavigationBar";
 import styled from "@emotion/styled";
+import Div100vh from "react-div-100vh";
 
-const MainLayout = styled(Box)`
-  width: 100%;
-  height: 100%;
-  min-height: 100%;
-  box-sizing: border-box;
-`;
+// const MainLayout = styled(Box)`
+//   width: 100%;
+//   box-sizing: border-box;
+// `;
 
 export default function Layout(props) {
   const { children } = props;
   return (
-    <MainLayout>
+    <Div100vh>
       <NavigationBar />
       <main style={{ padding: "1%" }}>{children}</main>
-    </MainLayout>
+    </Div100vh>
   );
 }
